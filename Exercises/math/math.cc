27c45a9bcea70b2512060fe3fc2467ac61b4af45
Mikkel <mikkelmb123@gmail.com>
2026-02-02 09:36:38 +0100

    math

--- Exercises/math/math.cc
+++ Exercises/math/math.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #define _USE_MATH_DEFINES // For math constants
 #include <cmath>
+#include "sfuns.h"
+
 
 int main() {
     double a = std::sqrt(2);
@@ -12,5 +14,10 @@ int main() {
     std::cout << "2^1/5 = " << b << std::endl;
     std::cout << "e^π = " << c << std::endl;
     std::cout << "π^e = " << d << std::endl;
+    std::cout << std::endl;
+
+    for (double i = 1; i <= 10; i++) {
+        std::cout << "Γ" << i << " = " << sfuns::fgamma(i) << std::endl;
+    }
     return 0;
 }
